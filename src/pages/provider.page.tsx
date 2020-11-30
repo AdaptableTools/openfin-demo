1c1bbd446545cd398d9adf2cf2a0d81716e1b4c1
roblotter <roblotter1@gmail.com>
2020-11-30 11:36:43 +0200

    add window position restore

--- src/pages/provider.page.tsx
+++ src/pages/provider.page.tsx
@@ -1,15 +1,56 @@
 import * as React from 'react';
 import { makeProvider } from '../components/provider';
 
+async function setLastSnapShot() {
+  const platform = fin.Platform.getCurrentSync();
+  const snapshot = await platform.getSnapshot();
+
+  if (snapshot.windows.length) {
+    const strSnapshot = JSON.stringify(snapshot);
+    localStorage.setItem('adaptable-layout', strSnapshot);
+  }
+}
+
+function getLastSnapShot() {
+  let snapshot: any = localStorage.getItem('adaptable-layout');
+  snapshot = snapshot ? JSON.parse(snapshot) : null;
+
+  if (snapshot) {
+    if (!snapshot.windows || !snapshot.windows.length) {
+      return null;
+    }
+  }
+  return snapshot;
+}
+
 function init() {
   fin.Platform.init({
     overrideCallback: async (Provider) => {
-      class Override extends Provider {}
+      class Override extends Provider {
+        async quit(...args) {
+          await setLastSnapShot();
+          return super.quit(...args);
+        }
+
+        async applySnapshot({ snapshot, options }, callerIdentity) {
+          const lastSnapshot = getLastSnapShot();
+          if (lastSnapshot) {
+            return super.applySnapshot(
+              { snapshot: lastSnapshot, options },
+              callerIdentity
+            );
+          } else {
+            return super.applySnapshot({ snapshot, options }, callerIdentity);
+          }
+        }
+      }
       return new Override();
     },
   });
 
   makeProvider();
+
+  setInterval(setLastSnapShot, 250);
 }
 
 const App: React.FC = () => {
