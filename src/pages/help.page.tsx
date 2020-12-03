7dd03cff82a3b63662342f4c6ef15f66a4e3f82f
jonnyadaptabletools <adaptabletools@JonnyAdaptableMacBook.local>
2020-12-03 17:43:36 +0200

    starting help page

--- src/pages/help.page.tsx
+++ src/pages/help.page.tsx
@@ -1,40 +1,34 @@
 import * as React from "react";
 
-
 const HelpPage: React.FC = () => {
   return (
     <>
-      <div style={{ padding: 20, overflow: 'auto' }}>
-        <h2>Guide</h2>
+      <div style={{ padding: 20, overflow: "auto" }}>
+        <h2>AdapTable - OpenFin Demo Guide</h2>
+        <h4>Overview</h4>
         <p>
-          Explain how it works
+          This demo application illustrates how{" "}
+          <a href="https://adaptabletools.com" target="_blank">
+            AdapTable
+          </a>{" "}
+          and{" "}
+          <a href="https://openfin.co/" target="_blank">
+            OpenFin
+          </a>
+          can be used together to provide a powerful, cutting-edge, feature-rich
+          application.{" "}
         </p>
         <p>
-          Ad reprehenderit qui nulla Lorem minim anim sit eu ad. Quis aliquip in nulla aliquip id cillum quis aute ea nulla consectetur est. Pariatur laboris officia et esse ullamco ex elit officia in pariatur veniam labore minim pariatur. Culpa do reprehenderit deserunt proident eu exercitation qui.
-
-          Est consequat quis proident ex dolore. Adipisicing magna Lorem esse aliqua cupidatat. Tempor culpa aute pariatur do id exercitation magna cillum laboris pariatur cillum. Labore anim qui laborum ad dolor mollit dolore esse commodo aliqua.
-          Mollit nisi deserunt ea minim. Aute nostrud minim magna reprehenderit dolor ad mollit fugiat do cupidatat ut tempor laboris eiusmod. Fugiat aliqua ut minim excepteur.
-
-          Ea amet cupidatat sunt proident tempor ea ad ex ex proident nostrud consectetur amet. In labore sint et ut esse ullamco commodo est velit. Elit sit incididunt est in velit do proident et consequat minim culpa occaecat. Ad ad cillum aliquip commodo incididunt fugiat ea proident dolor culpa proident laborum duis. Sunt pariatur non aliquip reprehenderit.
-
-          Excepteur mollit voluptate sit ut minim velit dolor excepteur dolore et dolor fugiat fugiat. Mollit culpa pariatur enim do qui. Adipisicing labore dolore enim laboris dolor ea labore ipsum veniam adipisicing aliqua cupidatat.
-
-          Cupidatat quis adipisicing reprehenderit eu cupidatat fugiat. Aute nisi incididunt pariatur enim aliqua sit tempor do velit exercitation mollit proident dolore consequat. Sint exercitation reprehenderit eu labore deserunt dolore sint non. Dolore enim aliquip voluptate id ut aliqua cupidatat sunt proident consequat et cupidatat nulla eu. Nisi incididunt ut deserunt veniam.
-
-          Tempor eiusmod aliqua mollit id officia do aliquip ea commodo elit sunt qui cillum. In ullamco mollit do pariatur sint cupidatat dolore sint id. Qui adipisicing amet esse minim aute et aute sunt. Sunt non ullamco do minim anim exercitation aliquip. Ut nisi dolor cupidatat sit velit adipisicing aute do adipisicing sint ad mollit sit.
-
-          Culpa pariatur sint proident commodo dolore ex exercitation dolor eiusmod ut id irure cupidatat pariatur. Do pariatur pariatur fugiat pariatur Lorem labore nostrud et enim id. Sit veniam non incididunt voluptate eu ipsum Lorem consectetur consectetur consectetur aliquip. Aliquip reprehenderit deserunt ut aliqua pariatur do minim. Est ea do cillum nisi non ad esse. Excepteur irure laboris laborum reprehenderit et reprehenderit reprehenderit velit.
+          It uses dummy data to mimic the types of screens workflows and
+          advanced use-cases typically found in Financial Services systems.
         </p>
         <p>
-          Exercitation consectetur nulla labore excepteur proident velit enim. Non adipisicing aliqua fugiat voluptate. Consectetur est ad eu in irure ut ullamco ad et est adipisicing id cupidatat duis. Aliqua ad in amet commodo ipsum sit culpa ullamco magna duis voluptate aute Lorem. Mollit velit tempor aliquip quis. Ea ipsum mollit fugiat magna sint magna nisi aliqua magna.
-
-          Fugiat eu amet do enim sit ipsum esse ullamco mollit ullamco quis dolore. Consectetur eu nostrud aliquip nostrud aliquip cupidatat quis in tempor officia sunt dolore cupidatat laboris. Ea consectetur dolor nostrud sunt magna magna deserunt sit id nulla cillum excepteur excepteur nisi. Veniam cupidatat esse fugiat aliquip anim nostrud occaecat Lorem consectetur proident quis non.
-
-          Quis quis nostrud sint tempor. Duis adipisicing tempor id exercitation. Non consectetur aute aliquip magna et amet in. Aliquip Lorem anim tempor deserunt veniam do laborum nostrud deserunt do amet id sint. Anim ut irure anim cupidatat et laboris. Elit ipsum pariatur consequat ipsum magna culpa tempor dolor enim sunt proident voluptate qui. Minim laborum consectetur pariatur incididunt labore cupidatat laboris do.
-
-          Laboris laborum nisi occaecat et. Sint aliqua ut sint officia reprehenderit cupidatat sunt id tempor eiusmod in. Qui veniam duis laborum et reprehenderit laboris ut nulla. Magna quis qui do cupidatat do. Dolore mollit non voluptate amet ad nostrud nisi nostrud. Voluptate officia voluptate aute amet duis incididunt non irure.
+          The application - which took less than a day to build - uses a small
+          subset of the many, exceptional features found in both AdapTable and
+          OpenFin (they are listed below).{" "}
         </p>
-
+        <h4>How it Works</h4>
+        <p>To Do</p>
       </div>
     </>
   );
