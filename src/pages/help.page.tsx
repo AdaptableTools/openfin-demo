f3206170768141a26c2ab54129943a1446792be5
jonnyadaptabletools <adaptabletools@JonnyAdaptableMacBook.local>
2020-12-04 08:26:41 +0200

    more Help

--- src/pages/help.page.tsx
+++ src/pages/help.page.tsx
@@ -37,7 +37,7 @@ const HelpPage: React.FC = () => {
           application.{" "}
         </p>
         <p>
-          It uses dummy data to mimic the types of screens workflows and
+          It uses <b>dummy data</b> to mimic the types of screens, workflows and
           advanced use-cases typically found in Financial Services systems.
         </p>
         <p>
@@ -46,17 +46,18 @@ const HelpPage: React.FC = () => {
         </p>
         <a id="how-works"></a>
         <h3>How it Works</h3>
-        <p>The Application mimics some typical Front Office workflows. </p>
         <p>
-          There are 3 main screens (each of which is an OpenFin application)
-          that show 'ticking' data and which update based on changes in the
-          other screens.{" "}
+          The Application displays a pseudo Front Office set-up via 3 main
+          screens - Trade, Price and Positions.
         </p>
-        <p>The 3 'ticking data' screens are:</p>
+        <p>
+          Each screen is an OpenFin application which shows 'ticking' data and
+          which updates based on changes in the other screens:
+        </p>
         <h4>1. Trade Blotter</h4>
         <ul>
           <li>
-            Displays a collection of dummy Trades, each of which has an{" "}
+            Displays a collection of fictitous Trades, each of which has an{" "}
             <i>InstrumentId</i>.
           </li>
           <li>
@@ -68,22 +69,26 @@ const HelpPage: React.FC = () => {
         <h4>2. Price Blotter</h4>
         <ul>
           <li>
-            Displays a fictitous list of instruments, each of which has a Price.
+            Displays a made-up list of <i>Instruments</i>, each of which
+            contains a Price.
           </li>{" "}
           <li>
             Every x seconds the Price is updated (and flashes accordingly).
           </li>{" "}
-          <li>The 'Bid Offer Spread' column sets the Ask and Bid</li>{" "}
+          <li>
+            Each entry also contains a Closing Price, Spread and Bid and Ask
+            values
+          </li>{" "}
         </ul>
         <h4>3. Positions Blotter</h4>
         <ul>
           <li>
-            Lists the positions for each <i>InstrumentId</i> based on the Trade
-            and Price screens.
+            Displays the positions for each <i>InstrumentId</i> based on data
+            from the Trade and Price screens.
           </li>
           <li>
-            Each row aggregates all the trades for an instrument and calculates
-            the PnL based on the current Price.
+            Each row aggregates all the trades for an <i>InstrumentId</i> and
+            calculates the PnL based on the current Price.
           </li>
           <li>
             Each time a Trade is added or a Price changes, the Positions Blotter
@@ -91,19 +96,21 @@ const HelpPage: React.FC = () => {
           </li>
         </ul>{" "}
         <h4>Keeping in Sync</h4>
-        <p>One nice feature in the Demo is that all 3 Blotters stay in sync.</p>
+        <p>
+          One essential feature in the Demo is that all 3 Blotters stay in sync.
+        </p>
         <p>
           For example, filtering on an Instrument in the Trade Blotter will
           cause the Price and Positions Blotters to filter immediately on the
           same instrument.
         </p>
         <p>
-          Likewise setting the Theme in one Blotter to dark will update all
-          other screens as well.
+          Likewise setting the Theme in one Blotter (e.g. to dark) will update
+          all other screens as well.
         </p>
         <p>
-          This can also be performed outside of the Grid by toggling the Theme
-          button at the top of the main window.
+          (This action can also be performed outside of the Grid by toggling the
+          Theme button at the top of the main window).
         </p>
         <h4>Notifications and Alerts</h4>
         <p>
@@ -121,21 +128,24 @@ const HelpPage: React.FC = () => {
           set to true (only available when running in the OpenFin container).
         </p>
         <p>
-          The result is that the Alert is fired as an{" "}
+          The result is that the Alert is displayed as an{" "}
           <b>OpenFin Notification</b> and appears in the [where?]?
         </p>
         <a id="audit-log"></a>
         <h3>Audit Screens</h3>
         <p>
-          The Demo leverages the{" "}
+          The Demo leverages the powerful{" "}
           <a
             href="https://docs.adaptabletools.com/docs/key-topics/audit-log"
             target="_blank"
           >
             AdapTable Audit Log
           </a>{" "}
-          to provide a live 'view' of all data changes. There are 2 Audit
-          Screens:
+          to provide a live 'view' of all data changes.{" "}
+        </p>
+        <p>
+          There are 2 Audit Screens - each of which listens to the Audit Log
+          stream and outputs to a new window:
         </p>
         <ul>
           <li>
@@ -152,28 +162,33 @@ const HelpPage: React.FC = () => {
         <a id="live-export"></a>
         <h3>Live Export</h3>
         <p>
-          AdapTable provides a number of unique, extra features, when it is
-          running in the OpenFin container.
+          AdapTable provides a number of compelling, extra features, only
+          available when it is running in the OpenFin container.
         </p>
         <p>
-          One of these is Live Export - whereby data can be sent from the Grid
-          to Excel with 2 way updates.
+          One of these is Live Export - whereby grid data can be sent from
+          AdapTable to Excel with 2 way updates:
         </p>
-        <p>
-          The result is that Excel will update in line with ticking data updates
-          in AdapTable, and any edits made in Excel will be automatically
-          reflected in the Blotter.
-        </p>
+        <ul>
+          <li>
+            Excel will automatically update in line with cell edits and ticking
+            data changes in AdapTable in AdapTable
+          </li>
+          <li>
+            Any date edits made directly in Excel will be automatically
+            reflected in AdapTable
+          </li>
+        </ul>
         <p>
           This can be achieved by selecting a report from the{" "}
-          <b>OpenFin Toolbar</b> in the Trades Blotter{" "}
+          <b>OpenFin Toolbar</b> in the Trades{" "}
           <a
             href="https://docs.adaptabletools.com/docs/user-interface/dashboard"
             target="_blank"
           >
             Dashboard
           </a>{" "}
-          and starting Live Update (the triangular buttton).
+          and running Live Update (the triangular buttton).
         </p>
         <a id="adaptable-features"></a>
         <h3>AdapTable Features</h3>
@@ -195,6 +210,10 @@ const HelpPage: React.FC = () => {
           <li>Flashing Cell</li>
           <li>Plus Minus</li>
           <li>Layout</li>
+          <li>Dashboard</li>
+          <li>Action Column</li>
+          <li>Export</li>
+          <li>User Interface - Edit lookup</li>
         </ul>
       </div>
     </>
