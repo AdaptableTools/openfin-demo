c8ed75cd59765ebeab45b6780a9ccb7dfd0e6f4d
roblotter <roblotter1@gmail.com>
2021-02-02 06:32:59 -0800

    add pack settings

---
+++ src/pages/pack-init/index.page.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const init = async () => {
+
+}
+const PackInit: React.FC = () => {
+    React.useEffect(() => {
+        init()
+    }, [])
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default PackInit;
