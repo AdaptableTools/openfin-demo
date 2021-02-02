2d9ee96b933bb386f2159df18016c1e10bed84cc
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2021-02-02 11:27:28 +0000

    adding validation to trade

--- src/pages/trade.tsx
+++ src/pages/trade.tsx
@@ -259,6 +259,7 @@ const adaptableOptions: AdaptableOptions = initAdaptableOptions({
     openfin({
       notificationTimeout: false,
       showAppIconInNotifications: true,
+      onValidationFailureInExcel: 'show-undo-notification'
       /*
 
       onExcelValidationFailed: (dataChangedInfo, failedValidationRules) => {
